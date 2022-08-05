76a46ef2d9c179e6037162463c2511b892a1fa69
minhieukt <minhieukt@gmail.com>
2022-08-05 09:45:07 +0700

    tao file caculator

---
+++ Js/caculator.js
@@ -0,0 +1,30 @@
+function add() {
+    let a = parseFloat(document.getElementById("firstNumber").value);
+    let b = parseFloat(document.getElementById("secondNumber").value);
+    document.getElementById("result").innerHTML = a + b;
+
+}
+
+function sub(){
+    let a = parseFloat(document.getElementById("firstNumber").value);
+    let b = parseFloat(document.getElementById("secondNumber").value);
+    document.getElementById("result").innerHTML = a - b;
+}
+
+function mul(){
+    let a = parseFloat(document.getElementById("firstNumber").value);
+    let b = parseFloat(document.getElementById("secondNumber").value);
+    document.getElementById("result").innerHTML = a*b;
+}
+
+function div(){
+    let a = parseFloat(document.getElementById("firstNumber").value);
+    let b = parseFloat(document.getElementById("secondNumber").value);
+    document.getElementById("result").innerHTML = a/b;
+}
+
+function caculator(){
+    let a = parseFloat(document.getElementById("firstNumber").value);
+    let b = parseFloat(document.getElementById("secondNumber").value);
+
+}
